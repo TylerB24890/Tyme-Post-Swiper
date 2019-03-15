5e4115e291803d0f92c05eb38a4eb0a6b0b274ba
Tyler Bailey <tyler.bailey@10up.com>
2019-03-15 15:49:25 -0400

    Add GNU license

--- tyme-post-swiper.php
+++ tyme-post-swiper.php
@@ -10,6 +10,12 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
  * @package tyme-post-swiper
+ *
+ * Copyright (C) 1989, 1991 Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
  */
 
 namespace Tyme\PostSwiper\Core;
