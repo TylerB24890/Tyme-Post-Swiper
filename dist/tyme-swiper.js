e18ae5d84e7c96113d7a8e8ef4780b409a489734
Tyler Bailey <tyler.bailey@10up.com>
2019-03-25 16:51:57 -0400

    Fix undefined error when post swiper block isn't active

--- dist/tyme-swiper.js
+++ dist/tyme-swiper.js
@@ -12,57 +12,59 @@ const initTymeSwiper = () => {
 // Get the swiper element
 	const swiperContainer = document.querySelector( '.swiper-container' );
 
-	// Swiper Transition Effect
-	const swiperEffect = swiperContainer.getAttribute( 'data-swiper-effect' );
+	if ( swiperContainer ) {
+		// Swiper Transition Effect
+		const swiperEffect = swiperContainer.getAttribute( 'data-swiper-effect' );
 
-	// Set the swiper options
-	let swiperOptions = {
-		autoplay: swiperContainer.getAttribute( 'data-swiper-autoplay' ),
-		loop: swiperContainer.getAttribute( 'data-swiper-loop' ),
-		slidesPerView: swiperContainer.getAttribute( 'data-swiper-perview' ),
-		centeredSlides: swiperContainer.getAttribute( 'data-swiper-centered' ),
-		effect: swiperEffect,
-		navigation: ( swiperContainer.getAttribute( 'data-swiper-navigation' ) ? {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		} : false ),
-		grabCursor: true,
-	};
-
-	// Set the Swiper Fade effect
-	if ( swiperEffect === 'fade' ) {
-		swiperOptions = {
-			...swiperOptions,
-			fadeEffect: {
-				crossFade: true,
-			},
-		};
-	}
-
-	// Configure the Swiper pagination
-	if ( swiperContainer.getAttribute( 'data-swiper-pagination' ) ) {
-	// The pagination type
-		const swiperPagiType = swiperContainer.getAttribute( 'data-swiper-pagination-type' );
-
-		swiperOptions = {
-			...swiperOptions,
-			pagination: {
-				el: '.swiper-pagination',
-				dynamicBullets: ( swiperPagiType === 'dynamic' ? true : false ),
-			},
+		// Set the swiper options
+		let swiperOptions = {
+			autoplay: swiperContainer.getAttribute( 'data-swiper-autoplay' ),
+			loop: swiperContainer.getAttribute( 'data-swiper-loop' ),
+			slidesPerView: swiperContainer.getAttribute( 'data-swiper-perview' ),
+			centeredSlides: swiperContainer.getAttribute( 'data-swiper-centered' ),
+			effect: swiperEffect,
+			navigation: ( swiperContainer.getAttribute( 'data-swiper-navigation' ) ? {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			} : false ),
+			grabCursor: true,
 		};
 
-		// If a pagination type is chosen other than the default bullets, set it.
-		if ( swiperPagiType !== 'default' && swiperPagiType !== 'dynamic' ) {
-			swiperOptions.pagination = {
-				...swiperOptions.pagination,
-				type: swiperPagiType,
+		// Set the Swiper Fade effect
+		if ( swiperEffect === 'fade' ) {
+			swiperOptions = {
+				...swiperOptions,
+				fadeEffect: {
+					crossFade: true,
+				},
 			};
 		}
-	}
 
-	// Initialize swiper
-	new Swiper( swiperContainer, swiperOptions );
+		// Configure the Swiper pagination
+		if ( swiperContainer.getAttribute( 'data-swiper-pagination' ) ) {
+		// The pagination type
+			const swiperPagiType = swiperContainer.getAttribute( 'data-swiper-pagination-type' );
+
+			swiperOptions = {
+				...swiperOptions,
+				pagination: {
+					el: '.swiper-pagination',
+					dynamicBullets: ( swiperPagiType === 'dynamic' ? true : false ),
+				},
+			};
+
+			// If a pagination type is chosen other than the default bullets, set it.
+			if ( swiperPagiType !== 'default' && swiperPagiType !== 'dynamic' ) {
+				swiperOptions.pagination = {
+					...swiperOptions.pagination,
+					type: swiperPagiType,
+				};
+			}
+		}
+
+		// Initialize swiper
+		new Swiper( swiperContainer, swiperOptions );
+	}
 };
 
 // Listen for the specified action above
